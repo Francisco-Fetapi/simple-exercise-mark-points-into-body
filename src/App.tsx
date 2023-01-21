2351d598b0224dc5c7e7a9a6833e403f69261e80
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-01-21 17:00:51 +0100

    feat: app finished with a littel bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,73 @@
-import { ButtonGroup, Button } from "@mui/material";
+import {
+  createTheme,
+  ButtonGroup,
+  Button,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import { useState } from "react";
 import "./App.css";
+import { MdRedo, MdUndo } from "react-icons/md";
+
+type DivOnClickHandle = React.MouseEventHandler<HTMLDivElement> | undefined;
+interface IMarks {
+  x: number;
+  y: number;
+  active: boolean;
+}
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  const [marks, setMarks] = useState<IMarks[]>([]);
+  const activeMarks = marks.filter((mark) => mark.active);
+
+  const markWithPoint: DivOnClickHandle = (e) => {
+    const [x, y] = [e.clientX, e.clientY];
+    setMarks((prev) => [{ x, y, active: true }, ...prev]);
+  };
+  function changeTheFirstMark(active: boolean) {
+    setMarks((prev) => {
+      const lastElementInactiveIndex = prev.findIndex(
+        (mark) => mark.active === active
+      );
+      if (lastElementInactiveIndex !== -1) {
+        const newMarks = [...prev];
+        newMarks[lastElementInactiveIndex].active = !active;
+        return newMarks;
+      }
+      return prev;
+    });
+  }
+  function redo() {
+    changeTheFirstMark(false);
+  }
+  function undo() {
+    changeTheFirstMark(true);
+  }
+
   return (
-    <div className="App">
-      <ButtonGroup variant="contained" color="primary">
-        <Button>Desfazer</Button>
-        <Button>Refazer</Button>
-      </ButtonGroup>
-      <div className="container"></div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <CssBaseline />
+        <ButtonGroup variant="contained" color="primary" className="btn-group">
+          <Button startIcon={<MdUndo />} onClick={undo}>
+            Desfazer
+          </Button>
+          <Button endIcon={<MdRedo />} onClick={redo}>
+            Refazer
+          </Button>
+        </ButtonGroup>
+        <div className="container" onClick={markWithPoint}>
+          {activeMarks.map(({ x, y }) => (
+            <div className="mark" style={{ top: y, left: x }} />
+          ))}
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
